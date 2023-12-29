7339d88f8a19983b602da236b19939043e5e4d64
baiyujie <2911906232@qq.com>
2023-12-29 16:48:45 +0800

    feat:加入商品到购物车+删除购物车数据

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const hotRouter = require('./router/hot')
 const goodsSort = require('./router/goodsSort')
 const goodsDetail = require('./router/goodsDetail')
 const avatarRouter = require('./router/uploadAvatar')
-const addressRouter =require('./router/address')
+const addressRouter = require('./router/address')
+const carRouter = require('./router/car')
 const app = express()
 const Joi = require('joi')
 const cors = require('cors')
@@ -17,38 +18,39 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // 挂载路由中间件
-app.use(userRouter) 
+app.use(userRouter)
 app.use(homeRouter)
 app.use(hotRouter)
 app.use(goodsSort)
 app.use(goodsDetail)
 app.use(avatarRouter)
 app.use(addressRouter)
+app.use(carRouter)
 
 // 配置全局错误 中间件
 app.use((err, req, res, next) => {
-    // 如果joi校验出现错误
-    if (err instanceof Joi.ValidationError) {
-        return res.send({
-            status: 1,
-            msg: err.message
-        })
-    }
-    // 身份校验错误
-    if (err.name === 'UnauthorizedError') {
-        return res.send({
-            status: 1,
-            msg: '身份认证失败'
-        })
-    }
-    // 其余未知错误
-    res.send({
-        status: 1,
-        msg: err.message
+  // 如果joi校验出现错误
+  if (err instanceof Joi.ValidationError) {
+    return res.send({
+      status: 1,
+      msg: err.message,
     })
+  }
+  // 身份校验错误
+  if (err.name === 'UnauthorizedError') {
+    return res.send({
+      status: 1,
+      msg: '身份认证失败',
+    })
+  }
+  // 其余未知错误
+  res.send({
+    status: 1,
+    msg: err.message,
+  })
 })
 
 // 监听服务器启动
 app.listen(8888, () => {
-    console.log('Server running at 127.0.0.1:8888')
+  console.log('Server running at 127.0.0.1:8888')
 })
